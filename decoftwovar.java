914e13d765d4b327be5c2fdf9ddc9384cec1555d
darshanparhyar <darshankumar.parhyar@gmail.com>
2022-04-14 18:25:07 +0500

    decofwar code

---
+++ decoftwovar.java
@@ -0,0 +1,12 @@
+public class decoftwovar {
+    public static void main(String [] args){
+        int distance=0;
+        int feet=0;
+        System.out.println("distance:"+ distance);
+        System.out.println("feet:"+ feet);
+
+
+
+    }
+    
+}
